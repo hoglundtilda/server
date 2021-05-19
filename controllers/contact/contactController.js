da44afa8b3c2e55a25dedfebc46089a7a82b1c5a
matilda.hoglund <hoglund.matilda@gmail.com>
2021-05-19 17:46:17 +0200

    Update contactController.js

--- controllers/contact/contactController.js
+++ controllers/contact/contactController.js
@@ -6,7 +6,7 @@ exports.validation = validate;
 
 exports.contact = async (req, res, err) => {
   const email = req.body.email;
-
+console.log(process.env.SMTP_PORT)
   //await createContactEmail(email);
   await createContactConfirmationEmail(email);
 
