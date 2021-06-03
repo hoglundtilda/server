8bb22139230fd1c79a5fcd317571b0897681263c
matilda.hoglund <hoglund.matilda@gmail.com>
2021-06-03 14:14:14 +0200

    Update transporter.js

--- controllers/modules/transporter.js
+++ controllers/modules/transporter.js
@@ -8,13 +8,13 @@ exports.transporter = nodemailer.createTransport({
    service: "Godaddy",
    host: "smtp.office365.com",
    port: process.env.SMTP_PORT,
-  secure: true,
+  secure: false,
   //requireTLS: true,
   auth: {
     user: process.env.ALEXANDER_MAIL,
     pass: process.env.ALEXANDER_PW,
   },
-  // tls: {
-  //   ciphers: "SSLv3",
-  // },
+  tls: {
+    ciphers: "SSLv3",
+  },
 });
