39f5f55b7c1a83e0ab90e4c43bd8ed99900360c6
matilda.hoglund <hoglund.matilda@gmail.com>
2021-06-08 19:12:23 +0200

    Update transporter.js

--- controllers/modules/transporter.js
+++ controllers/modules/transporter.js
@@ -1,19 +1,38 @@
 const nodemailer = require("nodemailer");
 
+// exports.transporter = nodemailer.createTransport({
+//   // service: "SendGrid",
+//   // host: "smtp.sendgrid.net",
+//   // port: 465,
+//   //service: "outlook.office365.com",
+//   host: "smtp.office365.com",
+//   port: 587,
+//   secureConnection: false,
+//   // requireTLS: true,
+//   tls: {
+//     // do not fail on invalid certs
+//     rejectUnauthorized: false,
+//     ciphers: "SSLv3",
+//   },
+//   auth: {
+//     user: process.env.KEVIN_MAIL,
+//     pass: process.env.KEVIN_PW,
+//   },
+// });
+
 exports.transporter = nodemailer.createTransport({
   // service: "SendGrid",
   // host: "smtp.sendgrid.net",
-  // port: 465,
-  //service: "outlook.office365.com",
+  //service: 'Godaddy',
   host: "smtp.office365.com",
   port: 587,
-  secureConnection: false,
+  secureConnection: true,
   // requireTLS: true,
-  tls: {
-    // do not fail on invalid certs
-    rejectUnauthorized: false,
-    ciphers: "SSLv3",
-  },
+  // tls: {
+  //   // do not fail on invalid certs
+  //   rejectUnauthorized: false,
+  //   ciphers: "SSLv3",
+  // },
   auth: {
     user: process.env.KEVIN_MAIL,
     pass: process.env.KEVIN_PW,
