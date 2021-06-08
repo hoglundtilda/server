b01a4c9d2e5502e16f34487ff338800b3b385f63
matilda.hoglund <hoglund.matilda@gmail.com>
2021-06-08 11:29:00 +0200

    Update transporter.js

--- controllers/modules/transporter.js
+++ controllers/modules/transporter.js
@@ -5,11 +5,11 @@ exports.transporter = nodemailer.createTransport({
   // service: "SendGrid",
   // host: "smtp.sendgrid.net",
   // port: 465,
-   service: "Godaddy",
+  // service: "Godaddy",
    host: "smtp.office365.com",
    port: process.env.SMTP_PORT,
   secure: false,
-  //requireTLS: true,
+  requireTLS: true,
   auth: {
     user: process.env.ALEXANDER_MAIL,
     pass: process.env.ALEXANDER_PW,
