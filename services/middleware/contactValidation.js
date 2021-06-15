12777ce74bcd5cbd918408998ebbd551ae4d6102
matilda.hoglund <hoglund.matilda@gmail.com>
2021-06-15 11:29:43 +0200

    validation

--- services/middleware/contactValidation.js
+++ services/middleware/contactValidation.js
@@ -3,8 +3,8 @@ const { body } = require("express-validator")
 exports.contactValidationRules = () => {
     
     return [
-        body('firstName').matches(/^[a-zA-ZåäöÅÄÖ]+$/),
-        body('lastName').matches(/^[a-zA-ZåäöÅÄÖ]+$/),
+        // body('firstName').matches(/^[a-zA-ZåäöÅÄÖ]+$/),
+        // body('lastName').matches(/^[a-zA-ZåäöÅÄÖ]+$/),
         body('email').matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
         body('subject').isLength({min: 2, max: 50}),
         body('message').isLength({min: 1}),
